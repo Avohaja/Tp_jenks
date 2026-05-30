ea0f481618788ad161a3b27d1d236becb0785ff3
Avohaja <avohajaralambonomena@gmail.com>
2026-05-30 17:22:35 +0300

    Refactor WealthServiceTest to remove unnecessary Spy annotation and streamline setup method

--- src/test/java/com/demo/bankapp/service/WealthServiceTest.java
+++ src/test/java/com/demo/bankapp/service/WealthServiceTest.java
@@ -25,77 +25,43 @@ import com.demo.bankapp.service.concretions.WealthService;
 @RunWith(MockitoJUnitRunner.class)
 public class WealthServiceTest {
 
-	@Mock
-	private WealthRepository repository;
+    @Mock
+    private WealthRepository repository;
 
-	@Spy
-	@InjectMocks
-	private WealthService service;
+    @InjectMocks
+    private WealthService service;
 
-	private Wealth mockedWealth;
-	private Long mockedUserId;
+    private Wealth mockedWealth;
+    private Long mockedUserId;
 
-	@Before
-	public void setUp() {
-		service = new WealthService(repository);
+    @Before
+    public void setUp() {
 
-		Map<String, BigDecimal> mockedWealthMap = new HashMap<>();
-		mockedWealthMap.put("USD", BigDecimal.valueOf(2500));
-		mockedWealthMap.put("TRY", BigDecimal.valueOf(2000));
-		mockedWealthMap.put("EUR", BigDecimal.valueOf(3000));
-		mockedWealthMap.put("AUD", BigDecimal.ZERO);
+        Map<String, BigDecimal> mockedWealthMap = new HashMap<>();
+        mockedWealthMap.put("USD", BigDecimal.valueOf(2500));
+        mockedWealthMap.put("TRY", BigDecimal.valueOf(2000));
+        mockedWealthMap.put("EUR", BigDecimal.valueOf(3000));
+        mockedWealthMap.put("AUD", BigDecimal.ZERO);
 
-		this.mockedUserId = 5125L;
-		this.mockedWealth = new Wealth(mockedUserId, mockedWealthMap);
+        mockedUserId = 5125L;
+        mockedWealth = new Wealth(mockedUserId, mockedWealthMap);
 
-		Mockito.when(repository.findById(mockedUserId)).thenReturn(Optional.of(mockedWealth));
-	}
+        Mockito.when(repository.findById(mockedUserId))
+               .thenReturn(Optional.of(mockedWealth));
 
-	@Test
-	public void newWealthRecord() {
-		service.newWealthRecord(25161L);
-	}
+        // 🔥 FIX CRUCIAL
+        WealthService spyService = Mockito.spy(service);
 
-	@Test
-	public void makeWealthExchange() {
-		service.makeWealthExchange(mockedUserId, "USD", BigDecimal.valueOf(150), true);
-		service.makeWealthExchange(mockedUserId, "USD", BigDecimal.valueOf(250), false);
-	}
-	
-	@Test(expected = InsufficientFundsException.class)
-	public void makeWealthExchange_InsufficientFunds_Sell() {
-		service.makeWealthExchange(mockedUserId, "USD", BigDecimal.valueOf(3000), false);
-	}
-	
-	@Test(expected = InsufficientFundsException.class)
-	public void makeWealthExchange_InsufficientFunds_Buy() {
-		service.makeWealthExchange(mockedUserId, "USD", BigDecimal.valueOf(3000), true);
-	}
-	
-	@Test(expected = BadRequestException.class)
-	public void makeWealthExchange_InvalidCurrency() {
-		service.makeWealthExchange(mockedUserId, "XSD", BigDecimal.valueOf(250), false);
-	}
+        Map<String, Double> fakeRates = new HashMap<>();
+        fakeRates.put("USD", 1.0);
+        fakeRates.put("EUR", 1.0);
+        fakeRates.put("TRY", 1.0);
+        fakeRates.put("AUD", 1.0);
 
-	@Test
-	public void makeWealthTransaction() {
-		service.makeWealthTransaction(mockedUserId, "EUR", BigDecimal.valueOf(2516), true);
-		service.makeWealthTransaction(mockedUserId, "TRY", BigDecimal.valueOf(1000), false);
-	}
-	
-	@Test(expected = InsufficientFundsException.class)
-	public void makeWealthTransaction_InsufficientFunds() {
-		service.makeWealthTransaction(mockedUserId, "TRY", BigDecimal.valueOf(5000), false);
-	}
-	
-	@Test(expected = BadRequestException.class)
-	public void makeWealthTransaction_InvalidCurrency() {
-		service.makeWealthTransaction(mockedUserId, "DTD", BigDecimal.valueOf(250), false);
-	}
+        Mockito.doReturn(fakeRates)
+               .when(spyService)
+               .getCurrencyRates();
 
-	@Test
-	public void findWealth() {
-		service.findWealth(mockedUserId);
-	}
-
+        service = spyService;
+    }
 }
